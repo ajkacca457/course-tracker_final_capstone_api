897443b7f57ddaafaefee2f2510a3dba419b569d
Avijit <ajkacca@gmail.com>
2021-03-08 16:40:39 +0200

    add validation to the models

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,9 @@
 class Course < ApplicationRecord
+
+  validates :name, presence:true
+  validates :number_of_lessons, presence:true
+  validates :lesson_completed, presence:true
+  validates :hours_needed, presence:true
+  validates :hours_spend, presence:true  
+
 end
