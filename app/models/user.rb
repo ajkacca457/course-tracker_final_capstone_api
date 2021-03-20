8c84f0c2e91075dfe35c9965442be2ecd0ce8529
Avijit <ajkacca@gmail.com>
2021-03-20 08:07:03 +0200

    add email validation with regex

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,11 @@ class User < ApplicationRecord
   has_many :trackers
   has_many :courses, through: :trackers, dependent: :destroy
 
+  REGEX_PATTERN = /\A[a-z0-9\+\-_\.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+
   validates :name, presence: true, length: { minimum: 6 }
   validates :username, presence: true, length: { minimum: 6 }
   validates :password, presence: true, length: { minimum: 6 }
-  validates :email, presence: true, length: { minimum: 6 }
+  validates :email, presence: true, format: { with: REGEX_PATTERN },
+                    uniqueness: true, length: { minimum: 5, maximum: 25 }
 end
